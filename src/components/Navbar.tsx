4497571facc2c27c090a10dff7598fffb89e2924
53d_luffy <junaid13h14@gmail.com>
2026-06-06 01:53:25 +0530

    Make navbar logo circle smaller and use standard natural alignment

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -135,8 +135,8 @@ export default function Navbar() {
       <div className="container mx-auto px-4 h-16 flex items-center justify-between">
         {/* Logo */}
         <Link href="/" className="flex items-center gap-3 group">
-          <div className="h-12 w-12 rounded-full border border-white/50 flex items-center justify-center flex-shrink-0 relative">
-            <img src="/MuftieAzam.png" alt="Logo" className="h-full w-full object-contain brightness-0 invert scale-[1.4] -translate-y-[2px] translate-x-[1px]" />
+          <div className="h-10 w-10 rounded-full border border-white/50 flex items-center justify-center flex-shrink-0 p-1">
+            <img src="/MuftieAzam.png" alt="Logo" className="h-full w-full object-contain brightness-0 invert" />
           </div>
           <span className="font-bold text-white text-xl hidden sm:block group-hover:opacity-90 transition-opacity">
             Islamic Scholarly Resource
